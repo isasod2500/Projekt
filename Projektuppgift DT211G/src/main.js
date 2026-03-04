1e63a18ed280577cfe7726658eadc2fb1383481a
isso2500 <isso2500@student.miun.se>
2026-03-04 20:53:53 +0100

    Basic functionality for webapp done.
    Added images to show ISS and Houston Space Station.
    
    Polyline shows "fågelvägen" to nearest brewery..

--- Projektuppgift DT211G/src/main.js
+++ Projektuppgift DT211G/src/main.js
@@ -4,45 +4,104 @@
  * Eventlyssnare för DOMContent, BreweryDB API och Open-Meteo API
  */
 document.addEventListener("DOMContentLoaded", async () => {
+   var map = L.map("map")
    try {
-    const callBrewery = await fetch("https://api.openbrewerydb.org/v1/breweries")
-    const gotBrewery = await callBrewery.json()
-    
-    createBrewery(gotBrewery)
-    
-    const callGeo = await fetch("https://geocoding-api.open-meteo.com/v1/search?")
-    const gotGeo = await callGeo.json();
 
-    createMap(gotGeo)
+     
 
-    const callSpace = await fetch("http://api.open-notify.org/iss-now.json")
-    const gotSpace = await callSpace.json();
+      const callGeo = await fetch("https://geocoding-api.open-meteo.com/v1/search?")
+      const gotGeo = await callGeo.json();
+      createMap(gotGeo)
+      map = await createMap(map)
+      
+      const callSpace = await fetch("http://api.open-notify.org/iss-now.json")
+      const gotSpace = await callSpace.json();
+      var spaceCoords = [
+         parseFloat(gotSpace.iss_position.latitude), 
+         parseFloat(gotSpace.iss_position.longitude)
+      ]
 
-    createSpace(gotSpace)
+       const callBrewery = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_dist=${spaceCoords}&per_page=200`)
+      const gotBrewery = await callBrewery.json()
+      console.log(gotBrewery)
+      var brewCoords = [gotBrewery[0].latitude, gotBrewery[0].longitude]
 
-    console.table(gotBrewery)
-    console.log(gotSpace.iss_position)
-    console.log(gotGeo)
+      document.getElementById("beerBtn").addEventListener("click", () => {
+         placeMarkers(spaceCoords, brewCoords, map)
+         showMarkers(spaceCoords, brewCoords, map)
+      })
 
-    document.getElementById("beerBtn").addEventListener("click", searchBrewery)
-   } catch(error) {
-    console.log(error)
+      return { spaceCoords, brewCoords, map};
+
+   } catch (error) {
+      console.log(error)
    }
+
 })
 
-function searchBrewery() {
+/**
+ * Placerar ut markörer utefter koordinater från fetch av iss-position och OpenBreweryDB
+ * @param {int} spaceCoords 
+ * @param {int} brewCoords 
+ * @param {var} map 
+ */
+function placeMarkers(spaceCoords, brewCoords, map) {
 
+   var spaceIcon = L.icon({
+      iconUrl:"../public/media/ISS.svg",
+
+      iconSize:   [50,50],
+      iconAnchor: [25, 25],
+      popupAnchor:[-3, -30]
+   });
+
+   
+   var spaceMarker = L.marker(spaceCoords, {icon: spaceIcon}).addTo(map).bindPopup("Här är du!")
+   var brewPopup = L.popup()
+   .setLatLng(brewCoords)
+      .setContent("Här är ölen!")
+      .addTo(map)
+
+   let comboCoords = [
+      spaceCoords,
+    brewCoords
+   ];
+   console.log(spaceCoords, brewCoords)
+   var path = L.polyline(comboCoords, {color: "#5C4900"}).addTo(map)
 }
 
-async function createBrewery(gotBrewery) {
-
+function showMarkers(spaceCoords, brewCoords, map) {
+   var bounds = new L.LatLngBounds(spaceCoords, brewCoords);
+   map.fitBounds(bounds, {padding:[50, 50]})
 }
+/**
+ * Skapar karta med Lyndon B Johnson Space Center centrerat.
+ */
+async function createMap(map) {
+   try {
+      map.setView([29.558221, -95.088997], 9);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         maxZoom: 19,
+         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
 
+      var popup = L.popup()
+      .setLatLng([29.558221, -95.088997])
+      .setContent("Houston, I am thirsty!!!!")
+      .openOn(map);
 
-async function createMap(gotGeo) {
+      var thirstyIcon = L.icon({
+      iconUrl:"../public/media/batteryLo.svg",
 
-}
+      iconSize: [35, 35],
+      iconAnchor:[19, 3],
+      popupAnchor:[-3, 10]
+   });
 
-async function createSpace(gotSpace) {
-  
+   var thirstyMarker = L.marker([29.558221, -95.088997], {icon: thirstyIcon }).addTo(map)
+
+      return map;
+   } catch (error) {
+      console.log(error);
+   }
 }
