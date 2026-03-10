b736fa9937264a488610290b560578b997d6766d
isso2500 <isso2500@student.miun.se>
2026-03-10 11:30:34 +0100

    Included vite image optimizer.

--- Projektuppgift DT211G/vite.config.js
+++ Projektuppgift DT211G/vite.config.js
@@ -1,5 +1,6 @@
-import {defineConfig} from "vite";
-import {resolve} from "path";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
     css: {
@@ -11,7 +12,18 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 inspiration: resolve(__dirname, "inspiration.html"),
-                about: resolve (__dirname, "about.html"),
+                about: resolve(__dirname, "about.html"),
             }
         }
-    }})
+    },
+    plugins: [
+        ViteImageOptimizer({
+            png: {
+                quality: 75
+            },
+            svg: {
+                quality: 75
+            }
+        })
+    ]
+})
